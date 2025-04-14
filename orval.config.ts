d3b7a833c4b6eb4e0f088288e04f9117c85ff720
Edward Upton <eau@netcraft.com>
2025-04-14 13:08:43 +0100

    Showcase issue with Orval duplicate hooks being generated

---
+++ orval.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "orval";
+
+export default defineConfig({
+  paginatedApi: {
+    input: {
+      target: "./openapi.yaml",
+    },
+    output: {
+      mode: "single",
+      target: "src/index.ts",
+      client: "react-query",
+      prettier: true,
+      override: {
+        operations: {
+          searchItems: {
+            query: {
+              useQuery: true,
+              useInfinite: true,
+              useInfiniteQueryParam: "cursor",
+            },
+          },
+        },
+      },
+    },
+  },
+});
